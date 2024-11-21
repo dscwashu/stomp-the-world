433e198a7598b4c57c5771caddb200b4e2115f86
Aidan Taubenblat-Roberts <a.taubenblat-roberts@wustl.edu>
2024-11-21 17:41:00 -0600

    Aidan

--- src/Components/OverviewHome/Overview.tsx
+++ src/Components/OverviewHome/Overview.tsx
@@ -3,28 +3,34 @@ import Image from 'next/image'
 
 export function Overview() {
   return (
-    <div className={styles['overview-block']}>
-      <div className={styles['overview-text']}>
-        <p>
-          Stomp the World serves students in preschool through high school in
-          the greater St. Louis area through:
-        </p>
-        <ul>
-          <li> Tutoring </li>
-          <li> Dance/movement </li>
-          <li> Travelling </li>
-          <li> After school programs </li>
-          <li> Character education </li>
-        </ul>
+    <section className={styles.overview}>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <h2 className={styles.heading}>Empowering Students in St. Louis</h2>
+          <p className={styles.description}>
+            Stomp the World serves students in preschool through high school in
+            the greater St. Louis area through comprehensive educational programs.
+          </p>
+          <ul className={styles.services}>
+            <li>One-on-One Tutoring</li>
+            <li>Dance & Movement Programs</li>
+            <li>Educational Travel Experiences</li>
+            <li>Enriching After School Programs</li>
+            <li>Character Development Education</li>
+          </ul>
+        </div>
+        
+        <div className={styles.imageWrapper}>
+          <Image
+            className={styles.image}
+            width={500}
+            height={600}
+            src='/OverviewPhoto.jpg'
+            alt='Students engaging in educational activities'
+            priority
+          />
+        </div>
       </div>
-
-      <div>
-        <Image
-          width={300}
-          height={350}
-          src='/OverviewPhoto.jpg'
-          alt='overview image'></Image>
-      </div>
-    </div>
+    </section>
   )
 }
